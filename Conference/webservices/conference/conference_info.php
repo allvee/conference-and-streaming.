ebf646f7657fd5b536921e643287c30a2cb6c4e9
allvee <alvee52@yahoo.com>
2016-02-01 15:58:57 +0600

    ok Scheduling

--- Conference/webservices/conference/conference_info.php
+++ Conference/webservices/conference/conference_info.php
@@ -7,8 +7,6 @@
  */
 
 $data = $_REQUEST;
-//exit( json_encode( array("alamin"=>"one","message"=>"No reasone","status"=>false) ));
-//echo json_encode( array($data) );
 
 require_once "../lib/config.php";
 require_once "../lib/common.php";
@@ -19,7 +17,6 @@ $cn = connectDB();
 
 
 $action = mysql_real_escape_string(htmlspecialchars($_REQUEST['action']));
-//$notification_channel = mysql_real_escape_string(htmlspecialchars($_REQUEST['notification_channel']));
 
 $data_info = isset($_REQUEST['info']) ? $_REQUEST['info'] : 'action';
 if ($data_info != 'action') {
@@ -27,27 +24,22 @@ if ($data_info != 'action') {
     $deleted_id = $data_info['action_id'];
 }
 
-//exit( json_encode( array($data_info) ));
-
 $tbl = "tbl_conference";
 $room_tbl = "tbl_conference_room";
 
 $is_error = 0;
 $last_updated = date('Y-m-d H:i');
-$last_updated_by = $_SESSION["UserID"];
 
 $start_array = array("00:01","00:31","01:01","01:31","02:01","02:31","03:01","03:31","04:01","04:31","05:01","05:31","06:01",
                     "06:31","07:01","07:31","08:01","08:31","09:01","09:31","10:01","10:31","11:01","11:31","12:01","12:31",
                     "13:01","13:31","14:01","14:31","15:01","15:31","16:01","16:31","17:01","17:31","18:01","18:31","19:01",
                     "19:31","20:01","20:31","21:01","21:31","22:01","22:31","23:01","23:31");
-//echo print_r($start_array);
-
 
 $end_array = array("00:30","01:00","01:30","02:00","02:30","03:00","03:30","04:00","04:30","05:00","05:30","06:00","06:30","07:00",
                 "07:30","08:00","08:30","09:00","09:30","10:00","10:30","11:00","11:30","12:00","12:30","13:00","13:30","14:00",
                 "14:30","15:00","15:30","16:00","16:30","17:00","17:30","18:00","18:30","19:00","19:30","20:00","20:30",
                 "21:00","21:30","22:00","22:30","23:00","23:30","11:59");
-//echo print_r($end_array);
+
         $valid_day=30;
 
 if ($action != 'delete') {
@@ -75,7 +67,6 @@ if ($action != 'delete') {
             $total_column= $total_column."`".$start_array[$i]."_".$end_array[$i]."`"."="."'Free'";
 
     }
-//echo print_r($total_column);
 
     $start = $start_date . " ".$start_time;
     $end  = $end_date." ".$end_time;
@@ -101,61 +92,6 @@ if ($action != 'delete') {
     }
     $day= "`Day`"." "."in"."(".$interval.")";
 
-
-   /* $to_time = strtotime($start_time);
-    $from_time = strtotime($end_time);
-    $minute= round(abs($to_time - $from_time) / 60,2);
-    $slot=($minute+1)/30;
-
-    $start_q = array();
-    $end_q = array();
-    $total_column= "";
-    $total_column_set= "";
-    $i=1;
-    array_push($start_q,"alamin");
-    array_push($end_q,"alamin");
-    array_push($start_q,$start_time);
-
-    // echo "start: ".print_r($start_q,1)."End:".print_r($end_q,1);
-     for($i=1; $i<=$slot; $i++)
-     {
-         $temp = "";
-         $start_time_split=explode(':', $start_q[$i]);
-          $start_time_split[1]= (int) $start_time_split[1]+29;
-         if((int) $start_time_split[1]>59)
-          {
-
-           $start_time_split[0]= (int) $start_time_split[0]+1;
-           if($start_time_split[0]<10)
-             $start_time_split[0]="0".(string) $start_time_split[0];
-             $start_time_split[1]="00";
-             $temp =  $start_time_split[0].":". $start_time_split[1];
-
-          }
-         else
-         {
-
-             $temp= $start_time_split[0].":". $start_time_split[1];
-
-         }
-
-         $end_q[$i] = $temp;
-         $end_time_split= explode(':', $end_q[$i]);
-         $end_time_split[1]= (int) $end_time_split[1]+1;
-
-         if((int)$end_time_split[1]<10 )
-             $end_time_split[1]="0".$end_time_split[1];
-
-         $start_q[$i+1] = $end_time_split[0]. ":".$end_time_split[1];
-         $column=$start_q[$i]."_" .$end_q[$i] ;
-
-         if(isset($total_column))
-            $total_column= $total_column. " AND "."`".$column."`" ."=". "'Free'";
-         else
-             $total_column= $total_column."`".$column."`" ."=". "'Free'";
-
-     }*/
-
     $dteStart = new DateTime($start);
     $dteEnd   = new DateTime($end);
     $dteDiff  = $dteStart->diff($dteEnd);
@@ -167,16 +103,11 @@ if ($action != 'delete') {
     $status= $response["status"];
     $long_code = $response["long_code"];
     $web_link = $response["web_link"];
-    //$room_pass = $response["room_pass"];
-    //$room_number = $response['room_number'];
-
-
     $_SESSION['long_code'] = $long_code;
 
     $schedule_conf = $data['schedule_conf_dropdown'];
 
     /*===================select room number from conference_scheduler DB ==============================*/
-
 
     if($schedule_conf=='Daily')
     {
@@ -238,15 +169,13 @@ if ($action != 'delete') {
 
 else
 {
-
-    /*=============================== for room number  for Delete =====================*/
+    /*===============================  for Delete =====================*/
     $action = $data_info['action'];
     $deleted_id = $data_info['action_id'];
     $room_number = $data_info['room_number'];
     $start_date_time = $data_info['start_date'];
     $end_date_time = $data_info['end_date'];
     $Schedule_Conf = $data_info['Schedule_Conf'];
-
 
     $date_time_split= explode(' ', $start_date_time);
     $date_split = explode('-', $date_time_split[0]);
@@ -258,7 +187,6 @@ else
     $start_time= $time_split[0].":".$time_split[1];
 
     $date_time_split= explode(' ', $end_date_time);
-
     $time_split = explode(':', $date_time_split[1]);
     $end_time= $time_split[0].":".$time_split[1];
 
@@ -277,13 +205,12 @@ else
     }
     $day= "`Day`"." "."in"."(".$interval.")";
 
-
     // echo json_encode( array("action"=>$action,"deleted_id"=>$deleted_id,"room_number"=>$room_number) );
 }
 
 
 if ($action == "update") {
-    $msg = "Successfully Updated";
+
     $action_id = mysql_real_escape_string(htmlspecialchars($_REQUEST['action_id']));
 
     $qry = "UPDATE $tbl set `Conf_Name`='$demo_name',`USER`='$user_id', `room_number`='$room_number', `weblink`='$web_link',
@@ -295,6 +222,9 @@ if ($action == "update") {
 
     $qry_to_room="UPDATE $room_tbl SET `room_pass`='$conference_code',`last_update` ='$last_updated', `conference_name` = '$demo_name' WHERE `room_number` ='$room_number'";
 
+
+
+    $msg = "Successfully Updated";
 }
 
 else if ($action == "delete") {
@@ -388,7 +318,6 @@ try {
 }
 
 /*================================== for conference id ======================================================*/
-
 if ($action == "save") {
     $qry_for_id = "SELECT ID FROM `tbl_conference` WHERE `Conf_Name`='$demo_name' and `long_number`= '$long_code' and `USER`='$user_id' and `room_number`='$room_number' and `weblink`='$web_link' and
                   `CODE`='$conference_code'and `Start_Time`='$start' and `End_Time`='$end' and `Participants`='$demo_participants' and `Recording`='$demo_recording' and
@@ -400,9 +329,9 @@ if ($action == "save") {
         $conf_id = Sql_Result($row, "ID");
     }
 
+    /*================================== Set conference id to Scheduler table ======================================================*/
     $_SESSION['conf_id'] = $conf_id;
     $total_column_set="";
-
     for($i=$key1; $i<=$key2; $i++)
     {
         $column=$start_array[$i]."_".$end_array[$i] ;
